b1cd9a63f3c31e9a53b62c3c749c96199a2167b5
Zach Lines <Zach.r.lines@gmail.com>
2026-01-05 00:23:36 +0000

    Ticket 3 - Soloution setup IBM Carbon + tailwind
    Setup project structure and made mini test page to ensure that my custom tailwind color classes and carbon are working together
    
    Link to issue https://github.com/Hopvine15/Temperature-Converter/issues/6
    
    Closes #6

---
+++ vite.config.ts
@@ -0,0 +1,27 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import path from 'path'
+
+export default defineConfig({
+  plugins:  [react()],
+  resolve: {
+    alias:  {
+      '@': path.resolve(__dirname, './src'),
+      '@components': path.resolve(__dirname, './src/components'),
+      '@utils': path.resolve(__dirname, './src/utils'),
+      '@types': path.resolve(__dirname, './src/types'),
+      '@hooks': path.resolve(__dirname, './src/hooks'),
+      '@context': path.resolve(__dirname, './src/context'),
+    },
+  },
+  css: {
+    preprocessorOptions:  {
+      scss: {
+        // Suppress deprecation warnings from Carbon
+        silenceDeprecations: ['legacy-js-api'],
+        quietDeps: true,
+      },
+    },
+  },
+  base: '/Temperature-Converter/', // For GitHub Pages
+})
